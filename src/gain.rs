7405eba992c924a98dbbc3ddaff06cf3bb925ab6
Patrick Simmelbauer <patrick.simmelbauer@gmail.com>
2026-04-30 21:12:57 +0200

    add ramp

--- src/gain.rs
+++ src/gain.rs
@@ -1,5 +1,37 @@
 use crate::node::Effect;
 
+pub struct Ramp {
+    target: f32,
+    start: f32,
+    duration_ms: u64,
+    start_time: Option<std::time::Instant>,
+}
+
+impl Ramp {
+    pub fn new(start: f32, target: f32, duration_ms: u64) -> Self {
+        Self {
+            target,
+            start, // This should be set to the current gain when ramp starts
+            duration_ms,
+            start_time: None,
+        }
+    }
+
+    pub fn start(&mut self) {
+        self.start_time = Some(std::time::Instant::now());
+    }
+
+    pub fn update(&mut self) -> f32 {
+        let Some(start_time) = self.start_time else {
+            return self.start; // Ramp hasn't started yet
+        };
+        let now = std::time::Instant::now();
+        let delta_time = now - start_time;
+        self.start
+            + (self.target - self.start) * (delta_time.as_millis() as f32 / self.duration_ms as f32)
+    }
+}
+
 pub struct Gain {
     gain: f32,
 }
--- src/main.rs
+++ src/main.rs
@@ -27,16 +27,20 @@ fn main() {
 
     let osc = Arc::new(Mutex::new(Oscillator::new(440.0, config.sample_rate())));
     let gain = Arc::new(Mutex::new(gain::Gain::default()));
+    let ramp = Arc::new(Mutex::new(gain::Ramp::new(1.0, 0.0, 1000)));
 
     let osc_arc = Arc::clone(&osc);
     let gain_arc = Arc::clone(&gain);
+    let ramp_arc = Arc::clone(&ramp);
 
     let stream = device
         .build_output_stream(
             &config.into(),
             move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+                let mut ramp = ramp_arc.lock().unwrap();
                 let mut osc = osc_arc.lock().unwrap();
                 let mut gain = gain_arc.lock().unwrap();
+                gain.set_gain(ramp.update());
                 for sample in data.iter_mut() {
                     *sample = gain.process(osc.output());
                 }
@@ -51,6 +55,6 @@ fn main() {
     stream.play().unwrap();
 
     std::thread::sleep(std::time::Duration::from_millis(1000));
-    gain.lock().unwrap().set_gain(0.5);
+    ramp.lock().unwrap().start();
     std::thread::sleep(std::time::Duration::from_millis(1000));
 }
